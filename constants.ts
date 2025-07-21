05fa38e01ce63774bc44fa96a292b5bcf8bf10ec
Adarsh Ajay <128365544+achuajays@users.noreply.github.com>
2025-07-20 23:39:03 -0700

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,3 +1,5 @@
+export const SUPABASE_URL = "https://tppghsndtbdsdgolpbbh.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwcGdoc25kdGJkc2Rnb2xwYmJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIyMDE4MTAsImV4cCI6MjA2Nzc3NzgxMH0.gOa_df0VrCWHHunbOUuFNh7PESwrIWHTaCG2jEs9U9Y";
 
 export const GENRES = [
   "Fantasy",
